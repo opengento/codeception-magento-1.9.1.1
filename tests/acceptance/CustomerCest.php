fcb89dc0e95ac3111633c938f34aba1c5dd4a31a
Frédéric MARTINEZ <frederic.martinez@ph2m.com>
2015-06-29 01:57:18 +0200

    Add Customer Create/Logout Cest v2

--- tests/acceptance/CustomerCest.php
+++ tests/acceptance/CustomerCest.php
@@ -10,7 +10,17 @@ class CustomerCest
     }
 
     public function createAccountAndLogout(AcceptanceTester $I){
+
         CustomerLoginPage::moveTo($I);
+        CustomerLoginPage::goToCreatePage($I);
+
+        CustomerCreatePage::amOn($I);
+        CustomerCreatePage::fillRegisterForm($I);
+
+        CustomerAccountIndexPage::amOn($I);
+        CustomerAccountIndexPage::logout($I);
+
+        HomePage::amOn($I);
     }
 
 
